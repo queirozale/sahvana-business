bd603c93752e956cfdade9531d01d8001fea75ca
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-03-30 21:40:15 -0300

    EditDialog fix

--- components/modules/Products/EditDialog.tsx
+++ components/modules/Products/EditDialog.tsx
@@ -50,8 +50,8 @@ function SimpleEditDialog(props: SimpleEditDialogProps) {
   const classes = useStyles();
   const { onClose, title, open } = props;
 
-  const handleClose = () => {
-    onClose();
+  const handleClose = (_event: any) => {
+    onClose(_event);
   };
 
 
