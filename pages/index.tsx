99913d4b1b67795cffe8b56253653f774acdc3b1
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-04-26 14:33:51 -0300

    LandingPage change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// import LandingPage from '../components/layouts/LandingPage';
-import LandingPage from '../components/layouts/LandingPageNew';
+import LandingPage from '../components/layouts/LandingPage';
+// import LandingPage from '../components/layouts/LandingPageNew';
 
 
 const IndexPage = () => {
