d232ee94bb3720da5d12304128588d776a369e1d
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-05-29 13:41:31 -0300

    Product update

--- components/modules/Products/Utils.js
+++ components/modules/Products/Utils.js
@@ -69,74 +69,16 @@ const createProduct = async (data) => {
   const result = await res.text();
 };
 
-// const createProduct = async event => {
-//   event.preventDefault();
-//   const form = event.target;
+const todayDateString = () => {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
 
-//   const optionList = (x) => {
-//     const optList = x.split(',');
-//     return optList.slice(0, optList.length - 1)
-//   };
+  today = yyyy + '-' + mm + '-' + dd;
 
-//   const inputOption1 = event.target.inputOption1? optionList(event.target.inputOption1.value) : [];
-//   const variantType1 = event.target.variantType1? event.target.variantType1.value : null;
+  return today;
+}
 
-//   const inputOption2 = event.target.inputOption2? optionList(event.target.inputOption2.value) : [];
-//   const variantType2 = event.target.variantType2? event.target.variantType2.value : null;
 
-//   const inputOption3 = event.target.inputOption3? optionList(event.target.inputOption3.value) : [];
-//   const variantType3 = event.target.variantType3? event.target.variantType3.value : null;
-
-//   var variantPrices = {}
-//   var variantInventories = {}
-
-//   optionValues.map(item => (
-//     variantPrices[item] = event.target['price_' + item].value
-//   ))
-
-//   optionValues.map(item => (
-//     variantInventories[item] = Number(event.target['inventory_' + item].value)
-//   ))
-
-//   const res = await fetch('https://sahvana-admin.herokuapp.com/api/create_product', {
-
-//     body: JSON.stringify({
-//       title: event.target.title.value,
-      // description: event.target.description.value,
-      // total_inventory: Number(event.target.inventory.value),
-      // original_price: event.target.original_price.value, 
-      // promotional_price: event.target.promotional_price.value,
-      // gender: event.target.gender.value,
-      // category: event.target.category.value,
-      // subcategory: event.target.subcategory.value,
-      // has_variant: event.target.has_variant.value,
-      // variantType1: variantType1,
-      // inputOption1: inputOption1,
-      // variantType2: variantType2,
-      // inputOption2: inputOption2,
-      // variantType3: variantType3,
-      // inputOption3: inputOption3,
-      // variantPrices: variantPrices,
-      // variantInventories: variantInventories,
-      // imageFiles: [
-      //   imageFile1,
-      //   imageFile2,
-      //   imageFile3,
-      //   imageFile4
-      
-      // ],
-      // created_at: new Date(),
-      // updated_at: new Date(),
-      // email: session.user.email
-//     }),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     method: 'POST'
-//   });
-
-//   const result = await res.text();
-//   Router.reload();
-// };
-
-export default { generateOptionsCombinations, createProduct };
+export default { generateOptionsCombinations, createProduct, todayDateString };
